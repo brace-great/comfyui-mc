3da8bd8b4151acf9fc13f65412239edad29a191a
brace-great <86789943+brace-great@users.noreply.github.com>
2025-05-14 13:59:43 +0800

    init

---
+++ encrypt_image.py
@@ -0,0 +1,29 @@
+class IncrementCounterOnMatch:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "target_value": ("INT", {"default": 0}),
+                "initial_counter": ("INT", {"default": 0}),
+                "input_value": ("INT",),
+            }
+        }
+
+    RETURN_TYPES = ("NUMBER", "STRING")  # Changed from ("INT", "STRING")
+    RETURN_NAMES = ("counter_int", "counter_str")
+    FUNCTION = "execute"
+
+    def execute(self, target_value, initial_counter, input_value):
+        if not hasattr(self, "counter"):
+            self.counter = initial_counter
+        if input_value == target_value:
+            self.counter += 1
+        return (
+            self.counter,
+            str(self.counter),
+        )  # counter is still an integer, compatible with NUMBER
+
+
+NODE_CLASS_MAPPINGS = {
+    "IncrementCounterOnMatch": IncrementCounterOnMatch,
+}
